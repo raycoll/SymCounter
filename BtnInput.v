71218e38b1e7e8f7e0ec053d8053331376139578
Steven Collison <stevendonv@gmail.com>
2015-05-20 15:44:14 -0700

    Removes ise comment

--- BtnInput.v
+++ BtnInput.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    15:010:08 04/29/2015
-// Design Name:
-// Module Name:    BtnInput
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module BtnInput(
    input clk,
 	 input btnU, // player count up
--- Clock.v
+++ Clock.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date:    14:55:29 04/27/2015
-// Design Name:
-// Module Name:    Clock
-// Project Name:
-// Target Devices:
-// Tool versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Clock(
     input Clk100M,
     input reset,
