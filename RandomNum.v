77ee09ae49d7d8049305e4f2578c3aa54e2787b1
Synthia Ling <synthialing@ucla.edu>
2015-06-01 15:01:26 -0700

    RandomNum using LFSR

--- RandomNum.v
+++ RandomNum.v
@@ -20,7 +20,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module RandomNum(
-    );
+  input clk,
+  input reset,
+  input enable,
+  output reg [3:0] randomNum
+ );
+ 
+ wire randBit;
+ assign randBit = ~(randomNum[3] ^ randomNum[2]);
+ 
+ always @(posedge clk or posedge reset) begin
+  if(reset)
+    randomNum=4'b0000
+  else
+    randomNum={randomNum[3:1], randBit}  //concat first three bits of randomNum to the random bit
+ end
 
 
 endmodule
