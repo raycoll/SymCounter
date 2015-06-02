5c46a1a20e5b32f718b871a3d9fe9461d5f14127
Steven Collison <stevendonv@gmail.com>
2015-06-01 22:36:49 -0700

    Adds intToSeg func to post/answer period

--- AnswerPeriod.v
+++ AnswerPeriod.v
@@ -11,6 +11,23 @@ module AnswerPeriod(
           output reg [7:0] answerSeg3
 );
 
+function [7:0] intToSeg;
+input [3:0] number;
+  case(number)
+    0: intToSeg=8'b11000000;
+    1: intToSeg=8'b11111001;
+    2: intToSeg=8'b10100100;
+    3: intToSeg=8'b10110000;
+    4: intToSeg=8'b10011001;
+    5: intToSeg=8'b10010010;
+    6: intToSeg=8'b10000010;
+    7: intToSeg=8'b11011000;
+    8: intToSeg=8'b10000000;
+    9: intToSeg=8'b10010000;
+    default: intToSeg=8'b11111111;
+  endcase
+endfunction
+
 // Current game time in seconds
 integer answerTime;
 reg periodFinished;
